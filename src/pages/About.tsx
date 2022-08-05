f2e9bc60fde4ef33a9d78be6dcb253654b8069b8
DimaBeley <2beyva@gmail.com>
2022-08-05 07:38:06 +0000

    add gsap and simple example

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,19 +1,47 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useRef, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
+import { gsap } from 'gsap'
 
 export const About: React.FC = () => {
   const history = useHistory()
+  const AboutRef = useRef<HTMLDivElement>(null)
+  const TextRef = useRef<HTMLDivElement>(null)
+  const ButtonRef = useRef<HTMLButtonElement>(null)
 
+  useEffect(() => {
+    gsap.from(AboutRef.current, {
+      opacity: 0,
+      y: 50,
+      duration: 1,
+    })
+    gsap.from(TextRef.current, {
+      opacity: 0,
+      y: 50,
+      duration: 1,
+      delay: 0.3,
+    })
+    gsap.from(ButtonRef.current, {
+      opacity: 0,
+      y: 50,
+      duration: 1,
+      delay: 0.6,
+    })
+  })
   return (
     <Fragment>
-      <h1>About</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-        possimus doloribus error cumque autem asperiores, ullam deserunt quidem
-        omnis doloremque itaque eius eaque sint facilis unde tenetur reiciendis
-        aliquam soluta?
-      </p>
+      <div ref={AboutRef}>
+        <h1>About</h1>
+      </div>
+      <div ref={TextRef}>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+          possimus doloribus error cumque autem asperiores, ullam deserunt quidem
+          omnis doloremque itaque eius eaque sint facilis unde tenetur reiciendis
+          aliquam soluta?
+        </p>
+      </div>
       <button
+        ref={ButtonRef}
         type="button"
         className="btn"
         cy-data="go-back-button"
